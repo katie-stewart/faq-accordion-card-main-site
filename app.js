67697599b18c428173b59cba8c2cbce9a6cfd03e
katiestewartcodes <97202985+katie-stewart@users.noreply.github.com>
2022-08-25 21:43:59 -0400

    added some neat Javascript functions

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const accordion = document.getElementsByClassName('faq');
 
 for (i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener('click', function() {
-        this.classList.toggle('active')
+        var isActive = this.classList.contains('active')
+        document.querySelectorAll('.faq').forEach(function(element) {
+            element.classList.remove('active')
+        })
+        if (isActive) {
+            this.classList.remove('active')
+        } else {
+            this.classList.add('active')
+        }
     })
 }
